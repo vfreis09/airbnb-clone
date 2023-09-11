9f98054245afe7a49ed49f58a88a99d64ab05242
vfreis09 <reis.vicente27@gmail.com>
2023-09-11 11:10:00 -0300

    navbar update

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import "../styles/Nav.css";
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <img src="./airbnb_logo.png" className="logo"></img>
+    </nav>
+  );
+}
+
+export default Nav;
